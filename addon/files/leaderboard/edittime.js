4232596c9dafff40f1ffbc19ed57933360269ae3
Henry Tejera <henrytejera@gmail.com>
2015-02-15 13:07:52 -0200

    Adding Construct2 addon

---
+++ addon/files/leaderboard/edittime.js
@@ -0,0 +1,119 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Leaderboard",
+		"id":			"Leaderboard",
+		"version":		"1.0",
+		"description":	"A self hosted Open source PHP Leaderboard. Post, fetch and display high scores",
+		"author":		"Ukelele studio",
+		"help url":		"http://ukelelestudio.com",
+		"category":		"Web",
+		"type":			"object",			
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0,	cf_trigger, "On Top Scores completed", "Leaderboard", "On Top Scores completed", "Triggered when a Top Scores request completes successfully.", "OnTopScoresComplete");
+AddCondition(1,	cf_trigger, "On Get Best Rank completed", "Leaderboard", "On Get Best Rank completed", "Triggered when a Get Best Rank request completes successfully.", "OnGetBestRankComplete");
+AddCondition(2,	cf_trigger, "On Get Last Rank completed", "Leaderboard", "On Get Last Rank completed", "Triggered when a Get Last Rank request completes successfully.", "OnGetLastRankComplete");
+AddCondition(3,	cf_trigger, "On Add Score completed", "Leaderboard", "On Add Score completed", "Triggered when a Add Score request completes successfully.", "OnAddScore");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddStringParam("Tag", "A tag, which can be anything you like, to distinguish between different Leaderboard requests.", "\"\"");
+AddFileParam("File", "Select a project file to request.");
+AddAction(1, 0, "Request project file", "Leaderboard", "Request <b>{1}</b> (tag <i>{0}</i>)", "Request a file in the project and retrieve its contents.", "RequestFile");
+
+AddNumberParam("Timeout", "The timeout for Leaderboard requests in seconds. Use -1 for no timeout.");
+AddAction(2, 0, "Set timeout", "Leaderboard", "Set timeout to <i>{0}</i> seconds", "Set the maximum time before a request is considered to have failed.", "SetTimeout");
+
+AddStringParam("Header", "The HTTP header name to set on the request.");
+AddStringParam("Value", "A string of the value to set the header to.");
+AddAction(3, 0, "Set request header", "Leaderboard", "Set request header <i>{0}</i> to <i>{1}</i>", "Set a HTTP header on the next request that is made.", "SetHeader");
+
+AddAction(4, 0, "Request Top Scores", "Leaderboard", "Request Top Scores", "Top Scores request returns an array of scores to your function where you can display the data in your Leaderboard.", "TopScores");
+
+AddStringParam("Player", "The player name.");
+AddAction(5, 0, "Get the player's last rank", "Leaderboard", "Get the last rank. Player: <b>{0}</b>", "Get the player's last rank.", "GetLastRank");
+
+AddStringParam("Player", "The player name.");
+AddAction(6, 0, "Get the player's best rank", "Leaderboard", "Get the best rank. Player: <b>{0}</b>", "Get the player's best rank.", "GetBestRank");
+
+AddStringParam("Player", "The player name.");
+AddNumberParam("Score", "The player's score.");
+AddAction(7, 0, "Submit score", "Leaderboard","Submit score. Player: <b>{0}</b> | Score: <b>{1}</b>", "Submit player's score.", "AddScore");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_string, "Get last data", "Leaderboard", "LastData", "Get the data returned by the last successful request.");
+AddExpression(1, ef_return_number, "Get progress", "Leaderboard", "Progress", "Get the progress, from 0 to 1, of the request in 'On progress'.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_text, "Leaderboard service URL","The Leaderboard URL.", "The Leaderboard URL."),
+	new cr.Property(ept_text, "Game ID", "", "The game id."),
+	new cr.Property(ept_text, "Magic number", "", "The magic number. Should be equal to the 'magic_number' property in the config.ini file"),
+	new cr.Property(ept_text, "Magic key", "", "The magic key. Should be equal to the 'magic_key' property in the config.ini file"),
+	new cr.Property(ept_combo, "Log requests", "False","Sends request URLs into console. For debugging purposes.", "False|True"),
+];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ addon/files/leaderboard/runtime.js
@@ -0,0 +1,524 @@
+﻿/** 
+ * A self hosted Open source PHP Leaderboard. Post, fetch and display high scores.
+ * Based on the AJAX plugin.
+ */
+// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Leaderboard = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var isNodeWebkit = false;
+	var path = null;
+	var fs = null;
+	var nw_appfolder = "";
+	
+	var pluginProto = cr.plugins_.Leaderboard.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		
+		this.lastData = "";
+		this.curTag = "";
+		this.progress = 0;
+		this.timeout = -1;
+				
+		isNodeWebkit = this.runtime.isNodeWebkit;
+		
+		if (isNodeWebkit)
+		{
+			path = require("path");
+			fs = require("fs");
+			nw_appfolder = path["dirname"](process["execPath"]) + "\\";
+		}
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+	
+	var theInstance = null;
+	
+	// For handling Leaderboard events in DC
+	window["C2_Leaderboard_DCSide"] = function (event_, tag_, param_)
+	{
+		if (!theInstance)
+			return;
+		
+		if (event_ === "success")
+		{
+			theInstance.curTag = tag_;
+			theInstance.lastData = param_;
+			theInstance.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnComplete, theInstance);
+			theInstance.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnTopScoresComplete, theInstance);
+			theInstance.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnGetLastRankComplete, theInstance);
+			theInstance.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnGetBestRankComplete, theInstance);			
+			theInstance.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnAddScore, theInstance);		
+		}
+		else if (event_ === "error")
+		{
+			theInstance.curTag = tag_;
+			theInstance.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnError, theInstance);
+		}
+		else if (event_ === "progress")
+		{
+			theInstance.progress = param_;
+			theInstance.curTag = tag_;
+			theInstance.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnProgress, theInstance);
+		}
+	};
+
+	instanceProto.onCreate = function()
+	{
+		theInstance = this;
+		this.url = this.properties[0];
+		this.gameId = this.properties[1];
+		this.magicNumber = this.properties[2];
+		this.magicKey = this.properties[3];
+		this.debug = this.properties[4];
+			
+		this.randomString = function(length) {
+			var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+			var result = '';
+			for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+			return result;
+		};
+				
+		this.encode64 = function (text) {
+			if (/([^\u0000-\u00ff])/.test(text)) {
+				throw new Error("Can't base64 encode non-ASCII characters.");
+			}
+			var digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
+				i = 0,
+				cur, prev, byteNum, result = [];
+			while (i < text.length) {
+				cur = text.charCodeAt(i);
+				byteNum = i % 3;
+				switch (byteNum) {
+				case 0:
+					result.push(digits.charAt(cur >> 2));
+					break;
+				case 1:
+					result.push(digits.charAt((prev & 3) << 4 | (cur >> 4)));
+					break;
+				case 2:
+					result.push(digits.charAt((prev & 0x0f) << 2 | (cur >> 6)));
+					result.push(digits.charAt(cur & 0x3f));
+					break;
+				}
+				prev = cur;
+				i++;
+			}
+			if (byteNum == 0) {
+				result.push(digits.charAt((prev & 3) << 4));
+				result.push("==");
+			} else if (byteNum == 1) {
+				result.push(digits.charAt((prev & 0x0f) << 2));
+				result.push("=");
+			}
+			return result.join("");
+		}
+		
+		this.decode64 = function(text) {
+			text = text.replace(/\s/g, "");
+			if (!(/^[a-z0-9\+\/\s]+\={0,2}$/i.test(text)) || text.length % 4 > 0) {
+				throw new Error("Not a base64-encoded string.");
+			}
+			var digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
+				cur, prev, digitNum, i = 0,
+				result = [];
+			text = text.replace(/=/g, "");
+			while (i < text.length) {
+				cur = digits.indexOf(text.charAt(i));
+				digitNum = i % 4;
+				switch (digitNum) {					
+				case 1:
+					result.push(String.fromCharCode(prev << 2 | cur >> 4));
+					break;
+				case 2:
+					result.push(String.fromCharCode((prev & 0x0f) << 4 | cur >> 2));
+					break;
+				case 3:
+					result.push(String.fromCharCode((prev & 3) << 6 | cur));
+					break;
+				}
+				prev = cur;
+				i++;
+			}
+			return result.join("");
+		}
+		
+		this.ord = function(string) {
+			var str = string + '',
+				code = str.charCodeAt(0);
+			if (0xD800 <= code && code <= 0xDBFF) {
+				var hi = code;
+				if (str.length === 1) {
+					return code;
+				}
+				var low = str.charCodeAt(1);
+				return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;
+			}
+			if (0xDC00 <= code && code <= 0xDFFF) {
+				return code;
+			}
+			return code;
+		}
+
+		this.encrypt = function(sData, sKey) {
+			var sResult = "";
+			var i = 0;
+			for (i = 0; i < sData.length; i++) {
+				var sChar = sData.substr(i, 1);
+				var sKeyChar = sKey.substr(i % sKey.length - 1, 1);
+				sChar = Math.floor(this.ord(sChar) + this.ord(sKeyChar));
+				sChar = String.fromCharCode(sChar);
+				sResult = sResult + sChar;
+			}
+			return this.encode64(sResult);
+		}
+		
+		this.doubleEncrypt = function(data){			
+			return this.encrypt(this.encrypt(data, this.magicKey), this.magicKey);		
+		}
+	};
+	
+	instanceProto.saveToJSON = function ()
+	{
+		return { "lastData": this.lastData };
+	};
+	
+	instanceProto.loadFromJSON = function (o)
+	{
+		this.lastData = o["lastData"];
+		this.curTag = "";
+		this.progress = 0;
+	};
+	
+	var next_request_headers = {};
+	var next_override_mime = "";
+	
+	instanceProto.doRequest = function (tag_, url_, method_, data_)
+	{
+		if(this.debug){
+			console.log("Leaderboard | Action:" + tag_ +  "| Request: " + url_);
+		}
+		// In directCanvas: forward request to webview layer
+		if (this.runtime.isDirectCanvas)
+		{
+			AppMobi["webview"]["execute"]('C2_Leaderboard_WebSide("' + tag_ + '", "' + url_ + '", "' + method_ + '", ' + (data_ ? '"' + data_ + '"' : "null") + ');');
+			return;
+		}
+		
+		// Create a context object with the tag name and a reference back to this
+		var self = this;
+		var request = null;
+		
+		var doErrorFunc = function ()
+		{
+			self.curTag = tag_;
+			self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnError, self);
+		};
+		
+		var errorFunc = function ()
+		{
+			// In node-webkit, try looking up the file on disk instead since it wasn't found in the project.
+			if (isNodeWebkit)
+			{
+				var filepath = nw_appfolder + url_;
+				
+				if (fs["existsSync"](filepath))
+				{
+					fs["readFile"](filepath, {"encoding": "utf8"}, function (err, data) {
+						if (err)
+						{
+							doErrorFunc();
+							return;
+						}
+						
+						self.lastData = data.replace(/\r\n/g, "\n")
+						self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnComplete, self);
+						self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnTopScoresComplete, self);
+						self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnGetLastRankComplete, self);
+						self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnGetBestRankComplete, self);
+						self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnAddScore, self);
+						
+					});
+				}
+				else
+					doErrorFunc();
+			}
+			else
+				doErrorFunc();
+		};
+			
+		var progressFunc = function (e)
+		{
+			if (!e["lengthComputable"])
+				return;
+				
+			self.progress = e.loaded / e.total;
+			self.curTag = tag_;
+			self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnProgress, self);
+		};
+			
+		try
+		{
+			// Windows Phone 8 can't Leaderboard local files using the standards-based API, but
+			// can if we use the old-school ActiveXObject. So use ActiveX on WP8 only.
+			if (this.runtime.isWindowsPhone8)
+				request = new ActiveXObject("Microsoft.XMLHTTP");
+			else
+				request = new XMLHttpRequest();
+			
+			request.onreadystatechange = function()
+			{
+				if (request.readyState === 4)
+				{
+					self.curTag = tag_;
+					
+					if (request.responseText)
+						self.lastData = request.responseText.replace(/\r\n/g, "\n");		// fix windows style line endings
+					else
+						self.lastData = "";
+					
+					if (request.status >= 400)
+						self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnError, self);
+					else
+					{
+						// In node-webkit, don't trigger 'on success' with empty string if file not found
+						if (!isNodeWebkit || self.lastData.length)
+							self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnComplete, self);
+							self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnTopScoresComplete, self);
+							self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnGetLastRankComplete, self);
+							self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnGetBestRankComplete, self);						
+							self.runtime.trigger(cr.plugins_.Leaderboard.prototype.cnds.OnAddScore, self);
+					}
+				}
+			};
+			
+			if (!this.runtime.isWindowsPhone8)
+			{
+				request.onerror = errorFunc;
+				request.ontimeout = errorFunc;
+				request.onabort = errorFunc;
+				request["onprogress"] = progressFunc;
+			}
+			
+			request.open(method_, url_);
+			
+			if (!this.runtime.isWindowsPhone8)
+			{
+				// IE requires timeout be set after open()
+				if (this.timeout >= 0 && typeof request["timeout"] !== "undefined")
+					request["timeout"] = this.timeout;
+			}
+			
+			// Workaround for CocoonJS bug: property exists but is not settable
+			try {
+				request.responseType = "text";
+			} catch (e) {}
+			
+			if (data_)
+			{
+				if (request["setRequestHeader"])
+				{
+					request["setRequestHeader"]("Content-Type", "application/x-www-form-urlencoded");
+				}
+			}
+			
+			// Apply custom headers
+			if (request["setRequestHeader"])
+			{
+				var p;
+				for (p in next_request_headers)
+				{
+					if (next_request_headers.hasOwnProperty(p))
+					{
+						try {
+							request["setRequestHeader"](p, next_request_headers[p]);
+						}
+						catch (e) {}
+					}
+				}
+				
+				// Reset for next request
+				next_request_headers = {};
+			}
+			
+			// Apply MIME type override if one set
+			if (next_override_mime && request["overrideMimeType"])
+			{
+				try {
+					request["overrideMimeType"](next_override_mime);
+				}
+				catch (e) {}
+				
+				// Reset for next request
+				next_override_mime = "";
+			}
+
+			if (data_)
+				request.send(data_);
+			else
+				request.send();
+			
+		}
+		catch (e)
+		{
+			errorFunc();
+		}
+	};
+	
+	/**BEGIN-PREVIEWONLY**/
+	instanceProto.getDebuggerValues = function (propsections)
+	{
+		propsections.push({
+			"title": "Leaderboard",
+			"properties": [
+				{"name": "Last data", "value": this.lastData, "readonly": true}
+			]
+		});
+	};
+	/**END-PREVIEWONLY**/
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+
+	Cnds.prototype.OnComplete = function (tag)
+	{		
+		return cr.equals_nocase(tag, this.curTag);
+	};
+	
+	Cnds.prototype.OnTopScoresComplete = function ()
+	{		
+		return cr.equals_nocase("TopScores", this.curTag);
+	};
+
+	Cnds.prototype.OnGetLastRankComplete = function ()
+	{		
+		return cr.equals_nocase("GetLastRank", this.curTag);
+	};	
+		
+	Cnds.prototype.OnGetBestRankComplete = function ()
+	{		
+		return cr.equals_nocase("GetBestRank", this.curTag);
+	};
+
+	Cnds.prototype.OnAddScore = function ()
+	{		
+		return cr.equals_nocase("AddScore", this.curTag);
+	};
+	
+	Cnds.prototype.OnError = function (tag)
+	{
+		return cr.equals_nocase(tag, this.curTag);
+	};
+	
+	Cnds.prototype.OnProgress = function (tag)
+	{
+		return cr.equals_nocase(tag, this.curTag);
+	};
+	
+	pluginProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+
+	Acts.prototype.TopScores = function ()
+	{
+		this.doRequest("TopScores",this.url + "/topscores/" + this.gameId, "GET");
+	};	
+
+	Acts.prototype.GetLastRank = function (player_)
+	{
+		this.doRequest("GetLastRank",this.url + "/getlastrank/" + this.gameId + "/" + player_, "GET");
+	};	
+
+	Acts.prototype.GetBestRank = function (player_)
+	{
+		this.doRequest("GetBestRank",this.url + "/getbestrank/" + this.gameId + "/" + player_ , "GET");
+	};	
+	
+	Acts.prototype.AddScore = function (player_, score_)
+	{
+		var random = this.randomString(3) + this.magicNumber + this.randomString(2);		
+		this.doRequest("AddScore",this.url + "/addscore/" + this.gameId + "/" + this.doubleEncrypt(random) + "/" + player_ + "/" + score_, "GET");
+	};	
+	
+	Acts.prototype.Request = function (tag_, url_)
+	{
+		this.doRequest(tag_, url_, "GET");
+	};
+	
+	Acts.prototype.RequestFile = function (tag_, file_)
+	{
+		this.doRequest(tag_, file_, "GET");
+	};
+	
+	Acts.prototype.Post = function (tag_, url_, data_, method_)
+	{
+		this.doRequest(tag_, url_, method_, data_);
+	};
+	
+	Acts.prototype.SetTimeout = function (t)
+	{
+		this.timeout = t * 1000;
+	};
+	
+	Acts.prototype.SetHeader = function (n, v)
+	{
+		next_request_headers[n] = v;
+	};
+	
+	Acts.prototype.OverrideMIMEType = function (m)
+	{
+		next_override_mime = m;
+	};
+	
+	pluginProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+
+	Exps.prototype.LastData = function (ret)
+	{
+		ret.set_string(this.lastData);
+	};
+	
+	Exps.prototype.Progress = function (ret)
+	{
+		ret.set_float(this.progress);
+	};
+	
+	pluginProto.exps = new Exps();
+
+}());
